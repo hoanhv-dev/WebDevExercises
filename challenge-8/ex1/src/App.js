674501e6aabcde984bd0b811ac95d44000519179
TuanKiettt <ntkiet.work@gmail.com>
2025-05-28 16:30:08 +0700

    8: Add Counter App and Login Form write by ReactJS

---
+++ challenge-8/ex1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Counter from './components/Counter';
+import LoginForm from './components/LoginForm';
+
+
+
+function App() {
+  return (
+    <>
+    <LoginForm />
+    <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ challenge-8/ex1/src/components/Counter.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  function handleCountPlus() {
+    setCount(count + 1);
+  }
+
+  function handleCountMinus() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-[120px] text-gray-800 mb-16">{count}</h1>
+
+      <div className="flex space-x-10">
+        <button
+          onClick={handleCountMinus}
+          className="w-50 h-50 bg-red-400 text-white rounded-xl shadow-lg hover:bg-red-500 transition-all duration-200 flex items-center justify-center text-3xl"
+        >
+          Minus
+        </button>
+
+        <button
+          onClick={handleCountPlus}
+          className="w-50 h-50 bg-green-400 text-white rounded-xl shadow-lg hover:bg-green-500 transition-all duration-200 flex items-center justify-center text-3xl"
+        >
+          Plus
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
