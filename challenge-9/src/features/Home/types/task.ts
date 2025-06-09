a360085fa7d0e5f66b1a76ded0a4a2661c153cf9
TuanKiettt <ntkiet.work@gmail.com>
2025-06-09 14:44:11 +0700

    9: remove duplication

--- challenge-9/src/features/Home/types/task.ts
+++ challenge-9/src/features/Home/types/task.ts
@@ -5,20 +5,6 @@ export type TasksType = {
   completed: boolean;
 };
 
-// Props for a single task item
-export type TaskItemProps = {
-  task: TasksType;
-  onToggle: (id: number) => void;
-  onDelete: (id: number) => void;
-};
-
-// Props for pagination
-export type PaginationProps = {
-  currentPage: number;
-  totalPages: number;
-  onPageChange: (page: number) => void;
-};
-
 // Props for the tasks list
 export type TasksListProps = {
   tasks: TasksType[];
